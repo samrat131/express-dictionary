27eb729d032a957fc7937e1744b256877aa427cf
samrat azad <samrat0131@gmail.com>
2021-03-30 02:39:52 +0600

    init

---
+++ app.js
@@ -0,0 +1,38 @@
+const fs =  require('fs')
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const readFile = ((req, res, next) => {
+  
+  console.log('reading file...')
+  
+  fs.readFile('./words.json', 'utf8', (err, data) => {
+    
+    if(err) console.log(err)
+    data = JSON.parse(data)
+    data.crowd = "vir kora"
+
+    data = JSON.stringify(data)
+    
+    fs.writeFile('./words.json', data, (err) => {
+      if(err) console.log(err)
+      console.log('file saved')
+    })
+
+    console.log(data)
+  })
+
+  next()
+})
+
+app.use(readFile)
+
+app.get('/', (req, res) => {
+  res.send('hello');
+})
+
+app.listen(port, ()=>{
+  console.log(`app started at ${port}`)
+})
